32a5636fb616c53f7a3a065676bc8303a01218c9
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-07-20 18:16:05 +0300

    added dart z package

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_e_commerce/core/errors/failure.dart';
 import 'package:fruit_e_commerce/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(String email, String password);
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password);
 }
