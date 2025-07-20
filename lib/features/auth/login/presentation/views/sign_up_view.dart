c923ce220c892801d9dad35165653161a37ac139
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-07-20 16:21:52 +0300

    register ui done

--- lib/features/auth/login/presentation/views/sign_up_view.dart
+++ lib/features/auth/login/presentation/views/sign_up_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_e_commerce/common/methods/build_app_bar.dart';
+import 'package:fruit_e_commerce/features/auth/login/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(context, 'Sign Up'));
+    return Scaffold(appBar: buildAppBar(context, 'Sign Up'), body: SignUpViewBody());
   }
 }
