8e42de37d60807b5a917fb107cb4d47061158314
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-07-20 19:04:38 +0300

    implement create user with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,4 +1,26 @@
-class FirebaseAuthService
-{
-  
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruit_e_commerce/core/errors/exceptions.dart';
+
+class FirebaseAuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (userCredential.user == null) {
+        throw CustomAuthException("فشل في إنشاء المستخدم.");
+      }
+
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      throw CustomAuthException.fromFirebaseAuthException(e.code);
+    }
+  }
 }
